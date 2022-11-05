6d41b8dd9b7050f1439171cbbbd1d78f7ff82932
장채윤 <chaeyoon-jang@jangchaeyun-ui-MacBookAir.local>
2022-11-05 12:33:00 +0900

    <add> excuteCode

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from mainapp.models import *
-
+import sqlite3
+import os
+import subprocess
 
 # request -> response : request handler
 # Pull data from db, Transform, Send an email, return HttpResponse
@@ -18,3 +20,16 @@ def initCode(request,input_data):
     result = Question.objects.filter(course=course,question=question)
     skeleton = result.skeleton
     return render(request,'hello.html',{'skeleton':skeleton})
+
+# excute python code
+def excuteCode(request, input_data):
+    code = input_data['code']
+    py = open('temp.txt','w')
+    py.write(code)
+    py.close()
+    os.rename('temp.txt','temp.py')
+    out = subprocess.Popen(['python','temp.py'], stdout=subprocess.PIPE).stdout  
+    return_data = out.read().strip()
+    out.close()
+    os.remove('temp.py') 
+    return render(request, 'hello.html', {'return_data':return_data})
