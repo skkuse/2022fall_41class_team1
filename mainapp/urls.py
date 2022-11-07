e238bc74575b4d765dcfad6c32610c72372c51b3
ChanuYu <vldpfh321@gmail.com>
2022-11-07 18:52:16 +0900

    Change url patterns to RE url patterns

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
+from django.urls import re_path
 from . import views
 
 #URLConf
 
 urlpatterns = [
-    path('user/',views.ListUser.as_view()),
-    path('user/<int:pk>/',views.DetailUser.as_view()),
+    re_path(r'user/',views.ListUser.as_view()),
+    re_path(r'user/(?P<userid>\w+)/',views.DetailUser.as_view()),
     
-    path('question/',views.ListQuestion.as_view()),
-    path('question/<int:pk>/',views.DetailQuestion.as_view()),
+    re_path(r'question/',views.ListQuestion.as_view()),
+    re_path(r'question/(?P<question>\w+)/',views.DetailQuestion.as_view()),
     
-    path('userdata/',views.ListUserData.as_view()),
-    path('userdata/<int:pk>/',views.DetailUserData.as_view()),
-    path('userdata/<int:pk>/results/',views.excuteCode)
+    re_path(r'userdata/',views.ListUserData.as_view()),
+    re_path(r'userdata/(?P<userid>\w+)/(?P<question>\w+)/',views.DetailUserData.as_view()),
+    re_path(r'userdata/(?P<userid>\w+)/(?P<question>\w+)/results/',views.excuteCode)
 ] 
-
