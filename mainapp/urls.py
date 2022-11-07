94ffe23c79f8e7c769176e3b18de992d3ff607a0
ChanuYu <vldpfh321@gmail.com>
2022-11-07 18:58:31 +0900

    Remove CSRF function in middleware

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -14,4 +14,4 @@ urlpatterns = [
     re_path(r'userdata/',views.ListUserData.as_view()),
     re_path(r'userdata/(?P<userid>\w+)/(?P<question>\w+)/',views.DetailUserData.as_view()),
     re_path(r'userdata/(?P<userid>\w+)/(?P<question>\w+)/results/',views.excuteCode)
-] 
+]
