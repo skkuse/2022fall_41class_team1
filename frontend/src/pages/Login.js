805b2dd61eb96c77005cd75464d70e6f71fc4600
yali98 <yali98@naver.com>
2022-11-30 23:53:32 +0900

    new

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import styles from "./Login.css";
 
 const Login = () => {
+
   const navigate = useNavigate();
 
   const onDesktop2Click = useCallback(() => {
