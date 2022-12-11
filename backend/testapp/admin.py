0f9c792bbdff907a3236d6c4888cd76f4a5c7d0e
ChanuYu <vldpfh321@gmail.com>
2022-12-11 13:50:46 +0900

    Reset all files

--- backend/testapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from testapp.models import *
-# Register your models here.
-
-admin.site.register(ExecuteCodeV1)
-admin.site.register(ExecuteCodeV2)
-admin.site.register(CheckTestcase)
-admin.site.register(EvaluateCode)
