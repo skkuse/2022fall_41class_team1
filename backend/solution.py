87d491c443ceededad51e4b94be27648387d0e6f
장채윤 <chaeyoon-jang@jangchaeyun-ui-MacBookAir.local>
2022-12-12 16:23:59 +0900

    <fix> commit error

--- backend/solution.py
+++ backend/solution.py
@@ -1,10 +1,6 @@
 # code에 함수 이거 넣어서 테스트 ㄱㄱ
 def solution(add1, add2, add3):
-<<<<<<< HEAD
 	sum = add1 + add2 + add3
-=======
-	sum = add1 + add2 + add3ㅁ
->>>>>>> 52394579090b943c816a9522bf74349be9b93b0d
 	return sum
 if __name__ == "__main__":
 	print(solution(1, 2, 3))
