f45dcb548003833445e0f3679a2e807efb2bb37d
ChanuYu <vldpfh321@gmail.com>
2022-12-12 20:41:33 +0900

    Merge ResultPage

--- frontend/src/pages/ResultPage.js
+++ frontend/src/pages/ResultPage.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./ResultPage.css";
 import { Link } from "react-router-dom";
@@ -12,26 +12,72 @@ import Play from "../assets/play.svg";
 import Chart, { drawCopy, drawFER } from "./Chart";
 
 const ResultPage = () => {
-  const [selected1, setSelected1] = useState(false);
-  const [selected2, setSelected2] = useState(false);
-  const [selected3, setSelected3] = useState(false);
+  let [titleName, setTitleName] = useState(["제출 결과 (Overall Score)"]);
+  const [overallInfo, setOverall] = useState({
+    총점: null,
+    표절: null,
+    기능: null,
+    효율: null,
+    가독성: null,
+  }); // 사진 + 정보 결과
+  const [detailInfo, setDetail] = useState(); // 정보만 결과
+  const [graphBlock, setGraphBlock] = useState(false);
+
+  const [selected1, setSelected1] = useState(false); //제출결과
+  const [selected2, setSelected2] = useState(false); //표절검사
+  const [selected3, setSelected3] = useState(false); //코드분석, 관련 링크
 
   const handle_button1 = () => {
     setSelected1(!selected1);
     setSelected2(false);
     setSelected3(false);
+
+    setTitleName("제출 결과 (Overall Score)");
   };
 
+  useEffect(() => {}, []);
+
   const handle_button2 = () => {
-    setSelected2(!selected2);
     setSelected1(false);
+    setSelected2(false);
     setSelected3(false);
+
+    setTitleName("가독성 채점");
+    setGraphBlock(false);
   };
 
   const handle_button3 = () => {
-    setSelected3(!selected3);
     setSelected1(false);
     setSelected2(false);
+    setSelected3(false);
+
+    setTitleName("기능 채점");
+    setGraphBlock(true);
+  };
+
+  const handle_button4 = () => {
+    setTitleName("효율 채점");
+    setGraphBlock(true);
+
+    setSelected1(false);
+    setSelected2(false);
+    setSelected3(false);
+  };
+  const handle_button5 = () => {
+    setTitleName("표절 검사");
+    setGraphBlock(true);
+
+    setSelected1(false);
+    setSelected2(!selected2);
+    setSelected3(false);
+  };
+  const handle_button6 = () => {
+    setTitleName("코드 설명, 관련 자료 채점");
+    setGraphBlock(false);
+
+    setSelected1(false);
+    setSelected2(false);
+    setSelected3(!selected3);
   };
 
   return (
@@ -48,37 +94,46 @@ const ResultPage = () => {
             </button>
             <button
               className="submitresult_innerbutton"
-              onClick={handle_button1}
+              onClick={handle_button2}
             >
               ㄴ 가독성 채점
             </button>
             <button
               className="submitresult_innerbutton"
-              onClick={handle_button1}
+              onClick={handle_button3}
             >
               ㄴ 기능 채점
             </button>
             <button
               className="submitresult_innerbutton"
-              onClick={handle_button1}
+              onClick={handle_button4}
             >
               ㄴ 효율 채점
             </button>
-            <button className="submitresult_button" onClick={handle_button2}>
+            <button className="submitresult_button" onClick={handle_button5}>
               <img src={Play} className="play"></img>
               표절 검사
             </button>
-            <button className="submitresult_button" onClick={handle_button3}>
+            <button className="submitresult_button" onClick={handle_button6}>
               <img src={Play} className="play"></img>
               코드 설명, 관련 자료
             </button>
           </div>
         </div>
         <div className="resultpage-right-container">
-          <div className="resultpage-rhdr "></div>
-          <div className="resultpage-component">
+          <div className="resultpage-rhdr ">{titleName}</div>
+          <div className="resultpage-component1">
             <div className="text_section">
               {selected1 ? drawFER() : selected2 ? drawCopy() : <div></div>}
+            </div>
+          </div>
+          <div className="resultpage-component2">
+            <div className="text_section">
+              <h1>
+                Result Text Area Result Text Area<br></br> Result Text Area
+                Result Text Area<br></br> Result Text Area Result Text Area
+                <br></br>{" "}
+              </h1>
             </div>
           </div>
         </div>
